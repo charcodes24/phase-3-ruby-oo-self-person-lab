5176fc64b8e9cf91fbde22d6df10922725bcbb6b
charcodes24 <charcodes24@gmail.com>
2021-07-15 09:25:28 -0700

    commit tree 98de43343018ccde5c911d9fe890a8e1bfb46a1c

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,65 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        if value > 10
+            @happiness = 10
+        elsif value < 0
+            @happiness = 0
+        else 
+            @happiness = value
+        end
+    end
+
+    def hygiene=(value)
+        if value > 10
+            @hygiene = 10
+        elsif value < 0
+            @hygiene = 0
+        else 
+            @hygiene = value
+        end
+    end
+
+    def happy?
+        @happiness > 7 ? true : false
+    end
+
+    def clean?
+        @hygiene > 7 ? true : false
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end 
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+    end
+end
